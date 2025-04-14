879fb39cddb9db5c8fb821e73b58a9ee00aac880
Karam <karam.taher56@gmail.com>
2025-04-14 18:25:35 +0300

    nothing

--- Backend/middlewares/checkSession.js
+++ Backend/middlewares/checkSession.js
@@ -4,14 +4,24 @@ const checkSession = (req, res, next) => {
     "/users/register",
     "/users/logout",
     "/users/login?expired=true",
-  ]
+  ];
+
   if (publicRoutes.includes(req.path)) {
     return next();
   }
-  if (req.session && req.session.userId) {
-    return next();
-  }
-  return res.redirect("/users/logout");
-}
 
-module.exports = checkSession;
+  if (!req.session || !req.session.userId) {
+    return res.redirect("/users/logout");
+  }
+
+  // if (req.path.startsWith("/admin")) {
+  //   if (!req.session.isAdmin) {
+
+  //     return res.status(403).send("Access Denied.");
+  //   }
+  // }
+
+  return next();
+};
+
+module.exports = checkSession
